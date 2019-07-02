7ced0062fc387b65a00ca636ca578fa33080f320
omq78 <omar.qab@gmail.com>
2019-07-02 22:53:03 +0300

    Initial Commit

---
+++ LoaderProgress/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoaderProgress
+//
+//  Created by Omar Alqabbani on 7/2/19.
+//  Copyright © 2019 OmarALqabbani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
