2d8a115c380cc511cd1a9a20c5e8e25adb64f12c
omq78 <omar.qab@gmail.com>
2019-07-03 02:04:04 +0300

    Stage1

--- LoaderProgress/ViewController.swift
+++ LoaderProgress/ViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
+let shapeLayer = CAShapeLayer()
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let circularPath = UIBezierPath(arcCenter: view.center, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: CGFloat.pi * 2, clockwise: true)
+        
+        shapeLayer.path = circularPath.cgPath
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.lineWidth = 10
+        shapeLayer.strokeEnd = 0
+        shapeLayer.lineCap = .round
+        
+        
+        let shapeTrack = CAShapeLayer()
+        shapeTrack.path = circularPath.cgPath
+        shapeTrack.lineWidth = 10
+        shapeTrack.strokeColor = UIColor.lightGray.cgColor
+        shapeTrack.fillColor = UIColor.clear.cgColor
+        
+        view.layer.addSublayer(shapeTrack)
+        view.layer.addSublayer(shapeLayer)
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleClickOnView)))
+        
     }
-
+    
+    @objc private func handleClickOnView(){
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.duration = 2
+        basicAnimation.toValue = 1
+        basicAnimation.fillMode = CAMediaTimingFillMode.forwards
+        basicAnimation.isRemovedOnCompletion = false
+        shapeLayer.add(basicAnimation, forKey: "urSoBasic")
+        
+    }
 
 }
 
